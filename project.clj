e38cc8e94dc5e57a3712d9f6ed5a7cef7317fd56
Ross Donaldson <gastove@gmail.com>
2015-10-12 14:46:39 -0700

    WOOOOO

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject formidable-blabs "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :description "Help I accidentally a Slack bot"
+  :url "http://github.com/Gastove/formidable-blabs"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [jarohen/chord "0.6.0]"]
   :main ^:skip-aot formidable-blabs.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
