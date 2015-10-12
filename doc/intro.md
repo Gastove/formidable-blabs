eecf1d94b0662e86456aed9d7e68258e23d9b97e
Ross Donaldson <gastove@gmail.com>
2015-10-12 11:39:40 -0700

    Init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to formidable-blabs
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/formidable_blabs/core_test.clj
@@ -0,0 +1,7 @@
+(ns formidable-blabs.core-test
+  (:require [clojure.test :refer :all]
+            [formidable-blabs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
