d858aebbe4e3c9fca804adbb34717250eb12558b
Ross Donaldson <gastove@gmail.com>
2015-10-13 23:10:00 -0700

    Deps; can open websocket w/ slack

--- project.clj
+++ project.clj
@@ -4,7 +4,12 @@
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [jarohen/chord "0.6.0]"]
+                 [jarohen/chord "0.6.0"]
+                 [jarohen/nomad "0.7.2"]
+                 [aleph "0.4.0"]
+                 [manifold "0.1.1"]
+                 [com.taoensso/timbre "4.1.4"]
+                 [org.clojure/core.match "0.3.0-alpha4"]]
   :main ^:skip-aot formidable-blabs.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/formidable_blabs/core.clj
+++ src/formidable_blabs/core.clj
@@ -1,5 +1,35 @@
 (ns formidable-blabs.core
-  (:gen-class))
+  (:require [aleph.http :as aleph]
+            [chord.http-kit :as http-kit]
+            [clojure.core.async :as async]
+            [clojure.data.json :as json]
+            [formidable-blabs.config :refer [blabs-config]]
+            [manifold.stream :as m]
+            [org.httpkit.client :as http]
+            [taoensso.timbre :as log]))
+
+(defn get-ws-url []
+  (let [url (:slack-start-url (blabs-config))
+        token (:slack-api-token (blabs-config))]
+    (http/get url {:query-params {:token token}})))
+
+(defn connect-to-slack []
+  (let [resp (get-ws-url)
+        body (json/read-str (:body @resp) :key-fn keyword)
+        ws-url (:url body)
+        socket-stream @(aleph/websocket-client ws-url)
+        chan (async/chan 10 (map #(json/read-str % :keyword keyword)))]
+    (m/connect socket-stream chan)
+    chan))
+
+(defmulti dispatch #(:type %))
+(defmethod dispatch "hello" [event]
+  (log/info "Got hello!"))
+(defmethod dispatch "message" [event])
+
+(defn consume-and-dispatch [in-ch]
+  (async/go-loop [body (async/<! in-ch)]
+    (dispatch body)))
 
 (defn -main
   "I don't do a whole lot ... yet."
