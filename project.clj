44149732b48dae529fdf59214e38efa1ee0d3df4
Ross Donaldson <gastove@gmail.com>
2015-10-17 11:47:37 -0700

    Remove unused dep, add cheshire + httpkit

--- project.clj
+++ project.clj
@@ -4,13 +4,14 @@
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [jarohen/chord "0.6.0"]
                  [jarohen/nomad "0.7.2"]
                  [aleph "0.4.0"]
                  [manifold "0.1.1"]
                  [com.taoensso/timbre "4.1.4"]
                  [org.clojure/core.match "0.3.0-alpha4"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [cheshire "5.5.0"]
+                 [http-kit "2.1.19"]]
   :main ^:skip-aot formidable-blabs.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
