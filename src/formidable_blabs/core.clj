1ef7186eb04c47c745a416a3616c9ea9b19a75d6
Ross Donaldson <gastove@gmail.com>
2015-10-17 11:45:28 -0700

    Refctor
    Tidies up the NS, moves things in to the `slack` ns.
    
    Closes #13 -- having all the processes happening in go-loops (that is,
    off the main proc) was causing the whole works to pass out and fall
    down. Moved process-outbound to the main thread and let it block,
    everything is happier now

--- src/formidable_blabs/core.clj
+++ src/formidable_blabs/core.clj
@@ -1,85 +1,49 @@
 (ns formidable-blabs.core
   (:gen-class)
-  (:require [aleph.http :as aleph]
-            [chord.http-kit :as http-kit]
-            [clojure.core.async :as async]
-            [clojure.data.json :as json]
-            [formidable-blabs.channels :refer [outbound-channel]]
-            [formidable-blabs.config :refer [blabs-config]]
-            [formidable-blabs.message-actions :refer [message-dispatch]]
-            [manifold.stream :as m]
-            [org.httpkit.client :as http]
+  (:require [clojure.core.async :as async :refer [go-loop]]
+            [formidable-blabs
+             [channels :refer [outbound-channel]]
+             [config :refer [blabs-config]]
+             [message-actions :refer [message-dispatch]]
+             [slack :refer [connect-to-slack make-message-sender]]]
             [taoensso.timbre :as log]))
 
-(defn get-ws-url []
-  (let [url (:slack-start-url (blabs-config))
-        token (:slack-api-token (blabs-config))]
-    (http/get url {:query-params {:token token}})))
-
-(defn connect-to-slack []
-  (let [resp (get-ws-url)
-        body (json/read-str (:body @resp) :key-fn keyword)
-        ws-url (:url body)
-        socket-stream @(aleph/websocket-client ws-url)
-        chan (async/chan 10 (map #(json/read-str % :key-fn keyword)))]
-    (m/connect socket-stream chan)
-    [socket-stream chan]))
-
-(defn send-message!
-  [[to-slack-channel message] message-id conn]
-  (let [body-map {:type "message"
-                  :text message
-                  :id message-id
-                  :channel to-slack-channel}
-        body-json (json/write-str body-map)]
-    (log/debug "Sending:" body-json)
-    (m/put! conn body-json)))
-
-(defn make-message-sender []
-  (let [id-cache (atom 0)]
-    (fn [chan-msg-pair conn]
-      (let [id (swap! id-cache inc)]
-        (send-message! chan-msg-pair id conn)))))
-
 (defmulti dispatch #(:type %))
-(defmethod dispatch "hello" [event]
-  (log/info "Got hello!"))
+(defmethod dispatch "hello" [_] (log/info "Got hello!"))
+(defmethod dispatch "pong" [_] (log/debug "Got pong."))
 (defmethod dispatch "message" [event] (message-dispatch event))
 (defmethod dispatch :default [event]
   ;; This logs a *lot* of stuff -- like keep-alives. Useful for
   ;; I'm-debugging-right-now debugging and nothing else.
-  ;; (log/debug "No :type key in event or event type unrecognized:" event)
+  (log/debug "No :type key in event or event type unrecognized:" event)
   )
 
 (defn consume-and-dispatch [in-ch]
-  (async/go-loop [body (async/<! in-ch)]
+  (go-loop [body (async/<! in-ch)]
+    (log/debug "Got from Slack:" body)
     (dispatch body)
     (if-let [next-item (async/<! in-ch)]
       (recur next-item)
       (log/debug "Got nil on in-ch, exiting."))))
 
-(defn tryit []
-  "Opens a connection to slack and starts running the whole works.
-
-  Returns a function that closes up the whole works when called."
-  (let [[sock ch] (connect-to-slack)
-        send! (make-message-sender)]
-    (consume-and-dispatch ch)
-    (async/go-loop [outgoing (async/<! outbound-channel)]
-      (send! outgoing sock)
-      (if-let [next-item (async/<! outbound-channel)]
-        (recur next-item)
-        (log/debug "Got nil on outbound channel, exiting.")))
-    (fn [] (m/close! sock) (async/close! ch) (async/close! outbound-channel))))
+(defn process-outbound
+  "Sends outgoing messages. Sends a ping every <config> seconds in which there
+  hasn't been any other traffic."
+  [out-ch sock]
+  (let [ping-millis (* (:ping (blabs-config)) 1000)
+        send! (make-message-sender sock)]
+    (loop []
+      (let [timeout-ch (async/timeout ping-millis)
+            [msg from-ch] (async/alts!! [out-ch timeout-ch])
+            ping {:type "ping"}]
+        (cond
+          (= from-ch out-ch) (send! msg)
+          (= from-ch timeout-ch) (do (log/debug "Sending ping") (send! ping))))
+      (recur))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [[sock ch] (connect-to-slack)
-        send! (make-message-sender)]
+  (let [[sock ch] (connect-to-slack)]
     (consume-and-dispatch ch)
-    (async/go-loop [outgoing (async/<! outbound-channel)]
-      (send! outgoing sock)
-      (if-let [next-item (async/<! outbound-channel)]
-        (recur next-item))))
-  (log/info "We're up!"))
+    (process-outbound outbound-channel sock)))
