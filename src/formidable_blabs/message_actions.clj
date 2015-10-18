4f5ca0caadef1bce175d328c94249d224142a44d
Ross Donaldson <gastove@gmail.com>
2015-10-18 13:47:52 -0700

    Wire up quotes, definitions

--- src/formidable_blabs/message_actions.clj
+++ src/formidable_blabs/message_actions.clj
@@ -6,7 +6,10 @@
             [clojure.edn :as edn]
             [clojure.java.io :as io]
             [formidable-blabs.channels :refer [outbound-channel]]
-            [taoensso.timbre :as log]))
+            [formidable-blabs.db :as db]
+            [formidable-blabs.slack :as slack]
+            [taoensso.timbre :as log]
+            [clojure.core.strint :refer [<<]]))
 
 ;; ### Message Actions
 ;; Actions based on either the sender of a message, the channel of a message,
@@ -30,17 +33,18 @@
 ;; !darkglasses
 ;; Hello / goodbye
 
+(defn send-msg-on-channel!
+  [slack-channel text]
+  (go (>! outbound-channel {:type "message" :channel slack-channel :text text})))
+
 (defn random-emote-by-key
   "Loads a set of responses by key out of resources/emotes.edn; returns a random
   emote"
   [k message emotes]
   (let [flips (k emotes)
         flip (rand-nth flips)
-        to-chan (:channel message)
-        out-msg {:type "message"
-                 :text flip
-                 :channel to-chan}]
-    (go (>! outbound-channel out-msg))))
+        to-chan (:channel message)]
+    (send-msg-on-channel! to-chan flip)))
 
 (declare remove-emoji-and-write! load-emoji-on-file)
 (defn random-emoji
@@ -95,7 +99,7 @@
     (let [n (rand-int 99)]
       (if (< n probability)
         (apply action args)
-        (log/debug "Rolled an '" n "', probability is " probability ", passing")))))
+        (log/debug "Rolled an" (str "'" n "'") " probability is:" probability ", passing")))))
 
 ;; ### Rate Limits
 ;; Some things shouldn't run all the time. This wrapper makes a function get
@@ -113,7 +117,6 @@
 
 ;; ### Check to see if it's time to do an action; if so, check its probability.
 (defn make-probabalistic-throttled-responder
-  ""
   [action probability throttle-seconds]
   (make-probabalistic-responder
    (make-throttled-responder action probability) throttle-seconds))
@@ -146,24 +149,101 @@
                      (partial random-emote-by-key :oops) (get-rate-limit :oops)))
 (def random-emoji-responder (make-probabalistic-responder random-emoji 24))
 
+;; ### Quotes
+;; Add a quote, search a quote by term, search a quote and return a specific result
+(defn add-quote!
+  [{:keys [text channel]}]
+  (if-let [[_ user quote-text] (re-find #"(?s)!q[uote]* add (\w+) (.+)" text)]
+    (do
+      (db/record-quote user quote-text)
+      (send-msg-on-channel! channel "Quote added!"))
+    (do
+      (send-msg-on-channel! channel "Erk! Something didn't work. One thousand apologies.")
+      (log/error "Either user or text not found?! That's effed up:" text))))
+
+(defn bounded-rand-int
+  [lower upper]
+  (loop [n (rand-int upper)]
+    (if (< n lower)
+      (recur (rand-int upper))
+      n)))
+
+(defn extract-quote-num
+  [text num-quotes]
+  (Integer/parseInt (or (second (re-find #"!q[uote]* \w+ (\d+)" text))
+                        (str (bounded-rand-int 1 num-quotes)))))
+
+(defn find-quote
+  [{:keys [text channel]}]
+  (if-let [[_ user-or-term] (re-find #"!q[uote]* (\w+)" text)]
+    (do (log/debug (re-find #"!q[uote]* \w+ (\d+)" text))
+        (let [result-seq (db/find-quote-by-user-or-term user-or-term)
+              num-quotes (count result-seq)
+              n (extract-quote-num text num-quotes)
+              ;; Vectors are zero-indexed, so nth accordingly.
+              q (nth result-seq (- n 1) (last result-seq))
+              {user :user quote-text :quote} q
+              msg (<< "~{user}: ~{quote-text} (~{n}/~{num-quotes})")]
+          (send-msg-on-channel! channel msg)))))
+
+;; ### Definitions
+(defn add-definition!
+  [{:keys [text channel]}]
+  (if-let [[_ term definition] (re-find #"(?s)!define (\w+): (.+)" text)]
+    (do
+      (db/record-definition term definition)
+      (send-msg-on-channel! channel (<< "Okay! `~{term}` is now defined as, `~{definition}`")))
+    (do
+      (send-msg-on-channel! "Erk! Something went wrong. I couldn't define that.")
+      (log/error "Couldn't get a definition out of:" text))))
+
+(defn send-define-help
+  [{:keys [text channel]}]
+  (send-msg-on-channel!
+   channel
+   "I didn't get that. To define a term, use the command format, `!define term: definition`"))
+
+(defn extract-definition-number
+  [text num-defs]
+  (Integer/parseInt (or (second (re-find #"(?s)!whatis .+ (\d+)" text))
+                        (str num-defs))))
+
+(defn third
+  [coll]
+  (nth coll 2))
+
+(defn find-definition
+  [{:keys [text channel]}]
+  (let [m (re-find #"(?s)!whatis (.+)\s\d+|!whatis (.+)" text)
+        term (or (second m) (third m))
+        result-seq (db/find-definiton-by-term term)
+        num-defs (count result-seq)
+        n (extract-definition-number text num-defs)
+        d (nth result-seq (- n 1) (last result-seq))
+        {defd-on :defined-at definition :definition} d
+        msg (<< "~{term}:\n> ~{definition}\n Definition ~{n} of ~{num-defs}; last defined ~{defd-on}")]
+    (send-msg-on-channel! channel msg)))
+
 ;; ### Dispatcher
 ;; **Remember:** Matching is done by `re-matches', which only matches if the _entire
 ;; string_ matches.
 (defn message-dispatch
-  ""
-  ;; username hack is in place for vetting emoji, revert to user when done
-  [{:keys [username text] :as message :or {text "" username ""}}]
+  "Uses regex matching to take a specified action on text."
+  [{:keys [user text] :as message :or {text "" user ""}}]
   (let [emotes (load-emotes)
-        emoji (load-all-emoji)]
+        emoji (load-all-emoji)
+        username (slack/get-user-name user)]
     (match [username text]
-           [_ #"(?s)!define.+"] (log/debug "'!wat' command not yet implemented")
-           [_ #"(?s)!whatis.+"] (log/debug "'!whatis' command not yet implemented")
-           [_ #"(?s)!quote.+"] (log/debug "'!quote' command not yet implemented")
            [_ #"!wat\s*"] (random-emote-by-key :wat message emotes)
            [_ #"!welp\s*"] (random-emote-by-key :welp message emotes)
            [_ #"!nope\s*"] (random-emote-by-key :nope message emotes)
            [_ #"!tableflip\s*"] (random-emote-by-key :tableflip message emotes)
            [_ #"(?i)[omf?g ]+\s*"] (omg-responder message emotes)
-           [_ #"(?i)[wh]?oops|uh-oh"] (oops-responder message emotes)
+           [_ #"(?i)[wh]*oops|uh-oh"] (oops-responder message emotes)
+           [_ #"(?s)!q[uote]* add \w+ .+"] (add-quote! message)
+           [_ #"!q[uote]* \w+\s?\d*"] (find-quote message)
+           [_ #"(?s)!define \w+: .+"] (add-definition! message)
+           [_ #"(?s)!define.+"] (send-define-help message)
+           [_ #"(?s)!whatis .+"] (find-definition message)
            [_ _] (random-emoji-responder message emoji)
            :else (log/debug "No message action found."))))
