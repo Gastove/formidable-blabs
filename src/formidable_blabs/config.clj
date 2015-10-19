0bcf7d46d27711a8427785f41687ce7cd9234917
Ross Donaldson <gastove@gmail.com>
2015-10-19 14:15:44 -0700

    Tidy up logging configs

--- src/formidable_blabs/config.clj
+++ src/formidable_blabs/config.clj
@@ -1,20 +1,12 @@
 (ns formidable-blabs.config
   (:require [clojure.java.io :as io]
-            [taoensso.timbre :refer [merge-config!]]
+            [taoensso.timbre :refer [set-config! set-level!]]
             [taoensso.timbre.appenders.core :as appenders]
             [nomad :as nomad]))
 
 (nomad/defconfig blabs-config (io/resource "config.edn"))
 
-(if (= "dev" (:nomad/environment (blabs-config)))
-  ;; dev configs
-  (do
-    (merge-config! {:appenders {:spit (appenders/spit-appender
-                                       {:fname "formidable.log"
-                                        :async? true})}}))
-  ;; production configs
-  (do
-    (merge-config! {:appenders {:spit (appenders/spit-appender
-                                       {:fname "formidable.log"
-                                        :async? true
-                                        :min-level :info})}})))
+
+(set-config! {:level (get-in (blabs-config) [:logging :level])
+              :appenders {:spit (appenders/spit-appender (get-in (blabs-config) [:logging :spit]))
+                          :print (appenders/println-appender (get-in (blabs-config) [:logging :spit]))}})
