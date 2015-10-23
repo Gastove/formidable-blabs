5c8ed90b9a61df00212e9c95955c4886425b4018
Ross Donaldson <gastove@gmail.com>
2015-10-22 21:35:56 -0700

    lein marg

--- project.clj
+++ project.clj
@@ -18,6 +18,7 @@
                  [org.xerial/sqlite-jdbc "3.7.2"]
                  [camel-snake-kebab "0.3.2"]
                  [org.clojure/core.incubator "0.1.3"]]
+  :plugins [[michaelblume/lein-marginalia "0.9.0"]]
   :main ^:skip-aot formidable-blabs.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
