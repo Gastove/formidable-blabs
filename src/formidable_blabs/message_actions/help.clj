1019778de698442fa32bbc6ad5aad45548b6a82e
Ross Donaldson <gastove@gmail.com>
2016-03-23 14:32:12 -0700

    Add config req; so many docs

--- src/formidable_blabs/message_actions/help.clj
+++ src/formidable_blabs/message_actions/help.clj
@@ -5,10 +5,27 @@
             [clojure.core.strint :refer [<<]]
             [clojure.java.io :as io]
             [formidable-blabs.slack :as slack]
+            [formidable-blabs.config :as config]
             [taoensso.timbre :as log]))
 
-;; TODO: move this to config, or something
-(def commands-map (edn/read-string (slurp (io/resource "commands.edn"))))
+;; ### The `help' ns
+
+;; This is Blabs' help system. It's interactive, and based on DMs (so it wont
+;; clog up the main chat channels Blabs is in; also for simplicity of
+;; implementation). Blabs loads its help from its main commands configuration
+;; file, commands.end, and wont offer help on any command without a `:help' key
+;; in its config map.
+;;
+;; In the future, it will be possible to (re-)define the help text for a command
+;; interactively, but at present that feature has yet to be implemented.
+;;
+;; #### Planned Improvements
+;; TODO: Allow (re-)defining help, interactively; maybe don't let just _any_
+;; user do this? Can check a users' admin flag, but using nomad for the config
+;; file makes this... interesting.
+;; TODO: Fuzzy-match help > strict text match help
+
+(def commands-map (config/commands))
 
 (def help-channels (atom {}))
 
@@ -16,30 +33,18 @@
   [user-id]
   (@help-channels user-id))
 
-(declare make-topic-list format-topic-list)
-(defn make-opening-message
+;; Generate the topic list from the loaded commands-map
+(defn make-topic-list
+  "Takes a map of commands of the format {command-name {command-spec}}
+  Returns the names of every command with a :help key defined."
   [commands]
-  (let [topics (-> commands
-                   (make-topic-list)
-                   (format-topic-list))]
-    (str/join \newline ["Hi! What can I help you with? Here's the commands I've got:"
-                        topics
-                        "Which command would you like to know more about? You can say a number or an exact term, or say \"done\" to end help."])))
-
-(defn start-help
-  [{user-id :user}]
-  (let [dm-channel-resp (slack/open-direct-message-channel user-id)
-        dm-channel (get-in dm-channel-resp [:channel :id])
-        opening-message (make-opening-message commands-map)]
-    (slack/post-message dm-channel opening-message)
-    (swap! help-channels assoc user-id dm-channel)))
-
-(defn make-topic-list [commands]
   (->> commands
        (filter (fn [[_ spec]] (not (nil? (:help spec)))))
        (keys)))
 
 (defn format-topic-list
+  "Takes the list of key names returned by `make-topic-list'.
+  Returns a string of topics formatted as `[Index of topic name] topic name'"
   [topic-list]
   (->> topic-list
        (map name)
@@ -47,22 +52,55 @@
        (map-indexed (fn [idx topic] (<< "[~{idx}] ~{topic}")))
        (str/join \newline)))
 
-(defn user-help-session-active?
-  [user]
-  (contains? @help-channels user))
+(defn make-opening-message
+  "Creates the initial message a user will receive on their DM channel with
+  Blabs when they initiate a help session."
+  [commands]
+  (let [topics (-> commands
+                   (make-topic-list)
+                   (format-topic-list))
+        opening "Hi! What can I help you with? Here's the commands I've got:"
+        closing (str "Which command would you like to know more about?"
+                     "You can say a number or an exact term, or say \"done\""
+                     "to end help.")]
+    (str/join \newline [opening topics closing])))
 
-(defn should-help?
-  [user channel]
-  (and (user-help-session-active? user)
+(defn start-help
+  "Initiates a help session. Opens a direct message channel, then
+  adds the pair `user-id dm-channel' to the help-channels atom."
+  [{user-id :user}]
+  (let [dm-channel-resp (slack/open-direct-message-channel user-id)
+        dm-channel (get-in dm-channel-resp [:channel :id])
+        opening-message (make-opening-message commands-map)]
+    (slack/post-message dm-channel opening-message)
+    (swap! help-channels assoc user-id dm-channel)))
+
+
+
+(defn user-help-session-active?
+  "Do we currently have a `user-id dm-channel' pair in the
+  help-channels atom for a given user-id?"
+  [user-id]
+  (contains? @help-channels user-id))
+
+(defn should-respond-with-help?
+  "We should respond with help if a user-id has an active help session
+  _and_ the channel we've received a message from that user on is
+  that users'  DM channel."
+  [user-id channel]
+  (and (user-help-session-active? user-id)
        (= (@help-channels user) channel)))
 
-(defn format-text
+(defn format-text-as-command-name
+  "Takes a string with a name like `find quote for name'
+  Returns a kebab-case keyword like `:find-quote-for-name'"
   [t]
   (-> t
       (str/replace #"\s" "-")
       (keyword)))
 
 (defn load-help-by-key
+  "Returns either the help text for a command, or a polite apology."
   [k]
   (if-let [help-text (get-in commands-map [k :help])]
     help-text
@@ -73,19 +111,21 @@
         k (nth (keys commands-map) idx)]
     (load-help-by-key k)))
 
-(defn get-help-by-string
-  [s]
-  (let [k (format-text s)]
+(defn get-help-by-string [s]
+  (let [k (format-text-as-command-name s)]
     (load-help-by-key k)))
 
 (defn end-help
+  "End the help session for a user by removing that users ID and dm-channel from
+  the help-channels atom"
   [user-id]
   (let [help-channel (get-help-channel-for-user user-id)]
     (slack/send-msg-on-channel! help-channel "Okie dokie!")
-    (swap! help-channel dissoc user-id)
-    (slack/close-direct-message-channel help-channel)))
+    (swap! help-channel dissoc user-id)))
 
 (defn dispatch-help
+  "Entry point for a current help session. Checks whether a user has provided a
+  numeric response or a text response and tries to provide help accordingly."
   [{message :msg}]
   (let [{:keys [user text]} message
         help-channel (get-help-channel-for-user user)]
